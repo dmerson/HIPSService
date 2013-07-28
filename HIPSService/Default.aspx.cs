2724bab880583873b7cb2130ed663851e027e182
don-E Merson <don.e.merson1966@gmail.com>
2013-07-27 22:21:40 -0700

    Got Encrypt working.

---
+++ HIPSService/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.UI;
+
+namespace HIPSService
+{
+    public partial class Default : Page
+    {
+        public int FirstThree(string SSN)
+        {
+            return int.Parse(SSN.Substring(0, 3));
+        }
+
+        public DateTime AddValue(DateTime dateToUser, int daysToAdd)
+        {
+            return dateToUser.AddDays(daysToAdd);
+        }
+
+        public int DateDifference(DateTime orignal, DateTime newDate)
+        {
+            return int.Parse((orignal -newDate).TotalDays.ToString());
+               
+        }
+        public string SpinSSN(string ssn, int spinNumber)
+        {
+            return (int.Parse(ssn) + spinNumber).ToString();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var ssn = "216821211";
+            var encrypt = new EncryptWithHips(ssn, new DateTime(1991, 1, 2), FirstThree, AddValue, new DateTime(1991, 1, 1), DateDifference, SpinSSN);
+            Response.Write(ssn + "<br>");
+            Response.Write(encrypt.RealizedPin +"<br>");
+            Response.Write(encrypt.NewDateOfBirth + "<br>");
+            Response.Write(encrypt.Stamp + "<br>");
+            
+            Response.Write(encrypt.NewSSN + "<br>");
+        }
+    }
+}
---
+++ HIPSService/EncryptWithHips.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HIPSService
+{
+    public class EncryptWithHips
+    {
+        public EncryptWithHips(string ssn, DateTime dateOfBirth, Func<string, int> PinFunction, Func<DateTime, int, DateTime> MakeNewSSNFunction, DateTime Key, Func<DateTime, DateTime, int> StampFunction, Func<string, int, string> SpinFunction)
+        {
+            RealizedPin = PinFunction(ssn );
+            NewDateOfBirth = MakeNewSSNFunction(dateOfBirth, RealizedPin );
+            Stamp = StampFunction(NewDateOfBirth, Key);
+            NewSSN = SpinFunction(ssn, Stamp);
+        }
+
+        public int RealizedPin { get; set; }
+        public DateTime NewDateOfBirth { get; set; }
+        public int Stamp { get; set; }
+        public string NewSSN { get; set; }
+
+        //private int GetRealizedKey(string ssn, Func<string, int> KeyFunction)
+        //{
+        //    return KeyFunction(ssn);
+        //}
+
+        //private DateTime GetNewDateFunc(DateTime realdate, int changeValue,
+        //                                Func<DateTime, int, DateTime> NewDateFunction)
+        //{
+        //    return NewDateFunction(realdate, changeValue);
+        //}
+    }
+
+
+    public class DecryptWithHIPS
+    {
+        public int RealizedPin { get; set; }
+        public DateTime RealDateOfBirth { get; set; }
+        public int Stamp { get; set; }
+        public string RealSSN { get; set; }
+        private int GetRealizedKey(string ssn, Func<string, int> KeyFunction)
+        {
+            return KeyFunction(ssn);
+        }
+        public DecryptWithHIPS(string encryptedSSN, DateTime encryptedDOB,Func<string, int> PinFunction )
+        {
+            RealizedPin = PinFunction(encryptedSSN);
+        }
+    }
+}
